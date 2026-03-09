e4ae5a28797a414130e384b6bb149d3981eb0311
sphamjoli <sphamjoli@users.noreply.github.com>
2026-03-09 12:57:48 +0000

    deploy: 43887c5f020b34b67e534d5dddfb984f5c3fa2cb

--- preview/pr-66/searcher-c2a407aa.js
+++ preview/pr-66/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c067f905.js',
+                path_to_root + 'searchindex-a5bf61c9.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
